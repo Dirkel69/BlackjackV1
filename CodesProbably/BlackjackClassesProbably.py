52a4321d8ff50e7e86ee14ec4474bc37069ccc37
Gilad <shvakim2015@gmail.com>
2024-06-16 22:26:37 +0300

    huh

--- CodesProbably/BlackjackClassesProbably.py
+++ CodesProbably/BlackjackClassesProbably.py
@@ -1,12 +1,22 @@
+import random
+
+
 class Card:
     value: int
     name: str
     number: int
-    sign: str
+    # I don't know if number will be used, so it's just here for now
     # Maybe add picture one day
+
+    def __init__(self, value: int, name: str, ):
+        self.value = value
+        self.name = name
 
 
 class CardPack:
+    card_options: list
+    used_cards: list
+
     def __init__(self):
         used_cards = []
         card_options = [
@@ -19,6 +29,43 @@ class CardPack:
             "club",
             "spade"
         ]
+
+    @staticmethod
+    def get_random_card(self):
+        result = self.generate_random_card()
+        while result in self.used_cards:
+            result = self.generate_random_card()
+        self.used_cards.append(result)
+        return result
+
+    def generate_random_card(self):
+        name = random.choice(self.card_options)
+        value = 0
+        if name == "ace":
+            value = 11
+            # number = 1
+        elif name == "king":
+            value = 10
+            # number = 13
+        elif name == "queen":
+            value = 10
+            # number = 12
+        elif name == "jack":
+            value = 10
+            # number = 11
+        elif name == "diamond":
+            value = random.randint(2, 11)
+            # number = 10
+        elif name == "heart":
+            value = random.randint(2, 11)
+            # number = 9
+        elif name == "club":
+            value = random.randint(2, 11)
+            # number = 8
+        elif name == "spade":
+            value = random.randint(2, 11)
+            # number = random.randint(2, 10)
+        return Card(value, name)
     # def GetRandomCard(self): check limit (add to used and see if over max)
 
 # The deck of 52 playing cards
